60be6ce7a5cb9c28cdb067816e70e89d6f7e9c83
antdp425 <antdp425@aol.com>
2020-05-01 11:41:20 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,8 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do #sets up connection to DB - located in db folder // named database.db
+   set :database, 'sqlite3:db/database.db'
+ end
+
 require './app'
