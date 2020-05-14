2af7deb36191ec56deafdee4b4818b95c6620f27
Kevin Mathias <komathias1@gmail.com>
2020-05-14 22:53:05 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,8 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do #sets up a connection to a sqlite3 databse named "database.db" but doesn't actually create the db
+  set :database, 'sqlite3:db/database.db'
+end
+
 require './app'
